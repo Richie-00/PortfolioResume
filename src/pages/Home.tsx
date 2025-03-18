cc6d939f42b77ecb58ba9420bf50d90bac867101
Richie-00 <itsrichiereal@gmail.com>
2025-03-18 22:24:00 +0800

    will this be the last

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import Navigation from "../components/Navigation"
 import Header from "../components/Header"
-import Links from "../components/Links"
+
 import Skill from "../components/Skill"
 import Footer from "../components/Footer"
 import Project from "../components/Image"
@@ -10,7 +10,7 @@ function Home() {
     <div>
       <Loading />
         <Navigation />  
-        <Links />
+      
         <Header />
         <Skill />
         <Project/>
